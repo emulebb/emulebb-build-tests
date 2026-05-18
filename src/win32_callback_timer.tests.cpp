6b6b73d08d78105293203fe29ba6f330753aee77
Threepwood-7 <mt0z1@pir0z.yxz>
2026-05-18 18:47:33 +0200

    REF-042 cover Win32 callback timer seams

---
+++ src/win32_callback_timer.tests.cpp
@@ -0,0 +1,44 @@
+#include "../third_party/doctest/doctest.h"
+
+#include "Win32CallbackTimerSeams.h"
+
+namespace
+{
+	void CALLBACK NoOpTimerProc(HWND, UINT, UINT_PTR, DWORD) noexcept
+	{
+	}
+}
+
+TEST_SUITE_BEGIN("parity");
+
+TEST_CASE("Win32 callback timer seam starts and clears null-window timers")
+{
+	UINT_PTR uTimerId = 0;
+
+	REQUIRE(Win32CallbackTimerSeams::TryStartNullWindowCallbackTimer(uTimerId, 1000u, NoOpTimerProc));
+	CHECK(uTimerId != 0);
+	CHECK(Win32CallbackTimerSeams::StopNullWindowCallbackTimer(uTimerId) == Win32CallbackTimerSeams::ETimerStopResult::Stopped);
+	CHECK(uTimerId == 0);
+	CHECK(Win32CallbackTimerSeams::StopNullWindowCallbackTimer(uTimerId) == Win32CallbackTimerSeams::ETimerStopResult::NotRunning);
+}
+
+TEST_CASE("Win32 callback timer dispatch guards preserve shutdown and visibility checks")
+{
+	CHECK(Win32CallbackTimerSeams::ShouldDispatchQueueListRefreshTimer(true, true, true, false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchQueueListRefreshTimer(false, true, true, false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchQueueListRefreshTimer(true, false, true, false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchQueueListRefreshTimer(true, true, false, false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchQueueListRefreshTimer(true, true, true, true));
+
+	CHECK(Win32CallbackTimerSeams::ShouldDispatchUploadQueueTimer(false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchUploadQueueTimer(true));
+
+	CHECK(Win32CallbackTimerSeams::ShouldDispatchServerRetryTimer(true));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchServerRetryTimer(false));
+
+	CHECK(Win32CallbackTimerSeams::ShouldDispatchUPnPTimeoutTimer(true, false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchUPnPTimeoutTimer(false, false));
+	CHECK_FALSE(Win32CallbackTimerSeams::ShouldDispatchUPnPTimeoutTimer(true, true));
+}
+
+TEST_SUITE_END();
