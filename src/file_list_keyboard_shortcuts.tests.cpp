09bbb23f9591351555b72b39e1b642b386cb1fab
Threepwood-7 <mt0z1@pir0z.yxz>
2026-05-23 13:04:10 +0200

    FEAT-064 cover list sort shortcuts

--- src/file_list_keyboard_shortcuts.tests.cpp
+++ src/file_list_keyboard_shortcuts.tests.cpp
@@ -45,6 +45,44 @@ TEST_CASE("file-list shortcuts leave unrelated and unsafe variants alone")
 	CHECK(FileListKeyboardShortcutsSeams::ClassifyKeyMessage(FileListKeyboardShortcutsSeams::EContext::SharedDirs, WM_KEYDOWN, 'F', true, false, false) == 0);
 }
 
+TEST_CASE("file-list sort shortcuts follow total-commander key ownership")
+{
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F3, true, false, false) == FileListKeyboardShortcutsSeams::ESortRole::Name);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F4, true, false, false) == FileListKeyboardShortcutsSeams::ESortRole::Type);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F5, true, false, false) == FileListKeyboardShortcutsSeams::ESortRole::Date);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F6, true, false, false) == FileListKeyboardShortcutsSeams::ESortRole::Size);
+}
+
+TEST_CASE("file-list sort shortcuts leave existing function-key behavior alone")
+{
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F3, false, false, false) == FileListKeyboardShortcutsSeams::ESortRole::None);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F5, false, false, false) == FileListKeyboardShortcutsSeams::ESortRole::None);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F3, true, true, false) == FileListKeyboardShortcutsSeams::ESortRole::None);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F3, true, false, true) == FileListKeyboardShortcutsSeams::ESortRole::None);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYUP, VK_F3, true, false, false) == FileListKeyboardShortcutsSeams::ESortRole::None);
+	CHECK(FileListKeyboardShortcutsSeams::ClassifySortKeyMessage(WM_KEYDOWN, VK_F7, true, false, false) == FileListKeyboardShortcutsSeams::ESortRole::None);
+}
+
+TEST_CASE("file-list sort shortcut roles map only to supported columns")
+{
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::Downloads, FileListKeyboardShortcutsSeams::ESortRole::Name) == 0);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::Downloads, FileListKeyboardShortcutsSeams::ESortRole::Type) == -1);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::Downloads, FileListKeyboardShortcutsSeams::ESortRole::Date) == 14);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::Downloads, FileListKeyboardShortcutsSeams::ESortRole::Size) == 1);
+
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SearchResults, FileListKeyboardShortcutsSeams::ESortRole::Name) == 0);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SearchResults, FileListKeyboardShortcutsSeams::ESortRole::Type) == 4);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SearchResults, FileListKeyboardShortcutsSeams::ESortRole::Date) == -1);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SearchResults, FileListKeyboardShortcutsSeams::ESortRole::Size) == 1);
+
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SharedFiles, FileListKeyboardShortcutsSeams::ESortRole::Name) == 0);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SharedFiles, FileListKeyboardShortcutsSeams::ESortRole::Type) == 2);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SharedFiles, FileListKeyboardShortcutsSeams::ESortRole::Date) == 6);
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SharedFiles, FileListKeyboardShortcutsSeams::ESortRole::Size) == 1);
+
+	CHECK(FileListKeyboardShortcutsSeams::GetSortShortcutColumn(FileListKeyboardShortcutsSeams::EContext::SharedDirs, FileListKeyboardShortcutsSeams::ESortRole::Name) == -1);
+}
+
 TEST_CASE("menu shortcut labels use native right-aligned menu hint format")
 {
 	CHECK(AddMenuShortcutLabel(CString(_T("Find")), _T("Ctrl+F")).Compare(_T("Find\tCtrl+F")) == 0);
