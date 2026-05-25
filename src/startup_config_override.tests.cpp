89b93a2346d859b15bc93466d3c878d37fc67899
Threepwood-7 <mt0z1@pir0z.yxz>
2026-05-25 13:28:13 +0200

    test: cover startup profile directory helpers

--- src/startup_config_override.tests.cpp
+++ src/startup_config_override.tests.cpp
@@ -10,7 +10,11 @@ TEST_CASE("Startup config override accepts canonical absolute drive paths")
 	CHECK(StartupConfigOverride::IsAbsoluteBaseDirPath(_T("C:\\profiles\\test-root\\")));
 	CHECK(StartupConfigOverride::NormalizeBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\")));
 	CHECK(StartupConfigOverride::GetConfigDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\config\\")));
+	CHECK(StartupConfigOverride::GetDataDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\")));
+	CHECK(StartupConfigOverride::GetTempDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\Temp\\")));
+	CHECK(StartupConfigOverride::GetIncomingDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\Incoming\\")));
 	CHECK(StartupConfigOverride::GetLogDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\logs\\")));
+	CHECK(StartupConfigOverride::GetExpansionDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\")));
 	CHECK(StartupConfigOverride::GetPreferencesIniPathFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\config\\preferences.ini")));
 }
 
