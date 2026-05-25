ef24c8223e68f456a668de2ae2b34810e3c497d2
Threepwood-7 <mt0z1@pir0z.yxz>
2026-05-25 13:56:24 +0200

    test: cover startup default incoming detection

--- src/startup_config_override.tests.cpp
+++ src/startup_config_override.tests.cpp
@@ -13,6 +13,9 @@ TEST_CASE("Startup config override accepts canonical absolute drive paths")
 	CHECK(StartupConfigOverride::GetDataDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\")));
 	CHECK(StartupConfigOverride::GetTempDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\Temp\\")));
 	CHECK(StartupConfigOverride::GetIncomingDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\Incoming\\")));
+	CHECK(StartupConfigOverride::IsDefaultIncomingDirectoryFromBaseDir(_T("C:\\profiles\\test-root"), _T("C:\\profiles\\test-root\\Incoming")));
+	CHECK(StartupConfigOverride::IsDefaultIncomingDirectoryFromBaseDir(_T("C:\\profiles\\test-root\\"), _T("c:\\profiles\\test-root\\incoming\\")));
+	CHECK_FALSE(StartupConfigOverride::IsDefaultIncomingDirectoryFromBaseDir(_T("C:\\profiles\\test-root"), _T("C:\\downloads\\incoming\\")));
 	CHECK(StartupConfigOverride::GetLogDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\logs\\")));
 	CHECK(StartupConfigOverride::GetExpansionDirectoryFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\")));
 	CHECK(StartupConfigOverride::GetPreferencesIniPathFromBaseDir(_T("C:\\profiles\\test-root")) == CString(_T("C:\\profiles\\test-root\\config\\preferences.ini")));
